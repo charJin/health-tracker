7d28feb04869fa502b34bcd28aedda814c9e8597
jinkim2739 <67286328+jinkim2739@users.noreply.github.com>
2025-03-24 23:59:12 -0700

    signup functionality

--- health-tracker/src/components/signup.tsx
+++ health-tracker/src/components/signup.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { UserAuth } from "../context/AuthContext";
 
@@ -10,19 +11,37 @@ const Signup = () => {
 	const [loading, setLoading] = useState("");
 
 	const auth = UserAuth();
+	const navigate = useNavigate();
 	if (!auth) return <div>Loading...</div>;
 	const { session, signUpNewUser } = auth;
+	console.log("Current Section: ", session);
 
-	console.log(session);
+	const handleSignUp = async (e) => {
+		e.preventDefault(); // stop page from reload
+		setLoading(true);
+		try {
+			const result = await signUpNewUser({ email, password });
+
+			if (result.success) {
+				navigate("/dashboard");
+			}
+		} catch (error) {
+			setError("an error occurred");
+		} finally {
+			setLoading(false);
+		}
+	};
+
 	return (
 		<div>
-			<form className="max-w-md m-auto pt-24">
+			<form onSubmit={handleSignUp} className="max-w-md m-auto pt-24">
 				<h2 className="font-bold pb-2">Sign up today!</h2>
 				<p>
 					Already have an account? <Link to="/signin">Sign in!</Link>
 				</p>
 				<div className="flex flex-col py-4">
 					<input
+						onChange={(e) => setEmail(e.target.value)}
 						placeholder="Email"
 						className="p-3 mt-2"
 						type="email"
@@ -30,6 +49,7 @@ const Signup = () => {
 						id=""
 					/>
 					<input
+						onChange={(e) => setPassword(e.target.value)}
 						placeholder="Password"
 						className="p-3 mt-2"
 						type="password"
@@ -39,6 +59,7 @@ const Signup = () => {
 					<button type="submit" disabled={loading} className="mt-6 w-full">
 						Sign up
 					</button>
+					{error && <p className="text-red-600 text-center pt-4">{error}</p>}
 				</div>
 			</form>
 		</div>
--- health-tracker/src/context/auth.types.ts
+++ health-tracker/src/context/auth.types.ts
@@ -2,12 +2,20 @@ import { Session, AuthError, AuthResponse } from "@supabase/supabase-js";
 
 export type AuthContextType = {
 	session: Session | null;
-	signUpNewUser: (
-		email: string,
-		password: string
-	) => Promise<{
+	signUpNewUser: (params: { email: string; password: string }) => Promise<{
 		success: boolean;
-		error?: AuthError;
+		error?: AuthError | unknown;
 		data?: AuthResponse["data"];
+	}>;
+
+	signInUser: (params: { email: string; password: string }) => Promise<{
+		success: boolean;
+		error?: AuthError | unknown;
+		data?: AuthResponse["data"];
+	}>;
+
+	signOut: () => Promise<{
+		success: boolean;
+		error?: AuthError | unknown;
 	}>;
 };
